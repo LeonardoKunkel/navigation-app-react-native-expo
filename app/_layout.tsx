1399909b3d3e341019bc92aec8c533d62c73e21a
Leonardo <mr_kunkel@hotmail.com>
2026-05-04 19:35:01 -0600

    Fonts added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,18 @@
-import { Slot } from 'expo-router';
 import "./global.css";
 
+import { useFonts } from "expo-font";
+import { Slot } from 'expo-router';
+
 const RootLayout = () => {
+
+    const [fontsLoaded] = useFonts({
+        "work-black": require("../assets/fonts/WorkSans-Black.ttf"),
+        "work-light": require("../assets/fonts/WorkSans-Light.ttf"),
+        "work-medium": require("../assets/fonts/WorkSans-Medium.ttf"),
+    });
+
+    if (!fontsLoaded) return null;
+
     return <Slot />
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 const App = () => {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-xl font-bold text-blue-500">
+            <Text className="text-xl font-bold text-blue-500 font-work-black">
                 Welcome to Nativewind!
             </Text>
         </View>
